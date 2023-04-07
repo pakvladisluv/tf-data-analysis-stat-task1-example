0aa9f022b6fa2c03a49c1221b374bcfee2131850
pakvladisluv <106927674+pakvladisluv@users.noreply.github.com>
2023-04-07 17:18:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,6 @@ import numpy as np
 chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = 93;
+    a = 2 * np.median(x) / (t ** 2)
+    return a # Ваш ответ
